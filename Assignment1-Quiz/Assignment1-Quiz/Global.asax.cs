b2eb30b03ba8473d8b3204dee81e860842af2d68
Brian Geraghty <s00077881@mail.itsligo.ie>
2015-03-13 18:33:56 +0000

    Fix

--- Assignment1-Quiz/Assignment1-Quiz/Global.asax.cs
+++ Assignment1-Quiz/Assignment1-Quiz/Global.asax.cs
@@ -9,10 +9,10 @@ namespace Assignment1_Quiz
 {
     public class Global : System.Web.HttpApplication
     {
-
+ 
         protected void Application_Start(object sender, EventArgs e)
         {
-
+           
         }
 
         protected void Session_Start(object sender, EventArgs e)
